6cba1c7b6af180bf0c51ed28620da45a2d990742
anandgitacc <anandgitacc>
2021-11-24 15:49:04 +0530

    firststage

---
+++ src/org/test/TestGit.java
@@ -0,0 +1,24 @@
+package org.test;
+
+public class TestGit {
+	
+public void empName() {
+		
+		System.out.println("anand");
+	}
+	
+	public void empMobno() {
+		
+		System.out.println("8939278836");
+	}
+	
+		
+public static void main(String[] args) {
+		
+	TestGit s = new TestGit();
+	
+	s.empName();
+	s.empMobno();
+}
+
+}
